f59d7f9a433526c0b9a0e7487c76a5a4ccc8d86a
lauryn-anderson <laurynelysse@gmail.com>
2020-06-24 12:32:16 -0600

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Lauryn Anderson on 2020-06-24.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
